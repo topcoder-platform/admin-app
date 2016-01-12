61e3f40c2aca4e944f7928e63fa1a4043340af47
ykohata <ykohata@appirio.com>
2016-01-12 15:52:43 +0900

    Copy activation link feature in Admin tool [COR-503] - fixed an agly source

--- src/app/users/constants.js
+++ src/app/users/constants.js
@@ -4,15 +4,15 @@ var module = angular.module('supportAdminApp');
 
 module.constant('users.Constants', {
     
-	MSG_CLIPBORD_TOOLTIP: "Copy to clipboard",
-	
-	MSG_CLIPBOARD_COPIED: "Copied",
-	
-	DICT_USER_STATUS: {
-      'A': 'Active',
-      'U': 'Unverified',
-      '4': 'Deactivated(User request)',
-      '5': 'Deactivated(Duplicate account)',
-      '6': 'Deactivated(Cheating account)'
-    }
+  MSG_CLIPBORD_TOOLTIP: "Copy to clipboard",
+
+  MSG_CLIPBOARD_COPIED: "Copied",
+
+  DICT_USER_STATUS: {
+    'A': 'Active',
+    'U': 'Unverified',
+    '4': 'Deactivated(User request)',
+    '5': 'Deactivated(Duplicate account)',
+    '6': 'Deactivated(Cheating account)'
+  }
 });
