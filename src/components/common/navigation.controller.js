ea14af8c06194f67d9e6e47c278f1c87c71901be
Qun Huang <huangqun@gmail.com>
2016-07-04 15:35:08 +0800

    fix user edit not working bug

--- src/components/common/navigation.controller.js
+++ src/components/common/navigation.controller.js
@@ -3,6 +3,7 @@
 angular.module('supportAdminApp')
   .controller('NavController', ['$scope', '$state', 
   function ($scope, $state) {
+    $scope.$state = $state;
 
     $scope.forceStateProjects = function () {
       $state.go('index.projects');
