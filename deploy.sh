a97f362a653c7d986f7e8d04cf0320c00ba102b1
Parth Shah <parth@topcoder.com>
2017-05-16 09:03:58 -0700

    updating deploy script to include public read option

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ BUCKET_URL=$2
 export AWS_ACCESS_KEY_ID=$(eval "echo \$${ENV}_AWS_ACCESS_KEY_ID")
 export AWS_SECRET_ACCESS_KEY=$(eval "echo \$${ENV}_AWS_SECRET_ACCESS_KEY")
 
-aws s3 sync dist s3://${BUCKET_URL} --delete
+aws s3 sync dist s3://${BUCKET_URL} --acl public-read --delete
