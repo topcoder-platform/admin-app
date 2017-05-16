59161726be13a75b77f16df2b349c756b07e9858
Parth Shah <parth@topcoder.com>
2017-05-16 09:34:23 -0700

    updating deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@
 echo "Deploying to S3"
 ENV=$1
 BUCKET_URL=$2
-export AWS_ACCESS_KEY_ID=$(eval "echo \$${ENV}_AWS_ACCESS_KEY_ID")
-export AWS_SECRET_ACCESS_KEY=$(eval "echo \$${ENV}_AWS_SECRET_ACCESS_KEY")
+AWS_ACCESS_KEY_ID=$(eval "echo \$${ENV}_AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY=$(eval "echo \$${ENV}_AWS_SECRET_ACCESS_KEY")
 
-aws s3 sync dist s3://${BUCKET_URL} --acl public-read --delete
+# aws s3 sync dist s3://${BUCKET_URL} --acl public-read --delete
+AWS_BUCKET=$BUCKET_URL AWS_KEY=$AWS_ACCESS_KEY_ID AWS_SECRET=$AWS_SECRET_ACCESS_KEY ./node_modules/.bin/gulp publish
