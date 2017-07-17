69fe712b8d1b0c3d11e37eb0a4b959cf52e6bdbc
Maksym Mykhailenko <maxceem@gmail.com>
2017-07-17 10:34:48 +0800

    challenge #30058339 - 48H ADMIN APP - SEARCH AND SORTING ON ROLES AND GROUPS PAGES

--- src/app/rolemembers/rolemembers.list.controller.js
+++ src/app/rolemembers/rolemembers.list.controller.js
@@ -3,8 +3,8 @@
 var module = angular.module('supportAdminApp');
 
 module.controller('permissionmanagement.RoleMembersListController', [
-              '$scope', '$rootScope', 'RoleService', 'IdResolverService', '$stateParams', '$state', '$q', 'Alert', '$timeout',
-    function ($scope, $rootScope, RoleService, IdResolverService, $stateParams, $state, $q, $alert, $timeout) {
+              '$scope', '$rootScope', 'RoleService', 'IdResolverService', 'UserService', '$animate', '$stateParams', '$state', '$q', 'Alert', '$timeout',
+    function ($scope, $rootScope, RoleService, IdResolverService, UserService, $animate, $stateParams, $state, $q, $alert, $timeout) {
 
       // true if role is loading
       $scope.isLoading = false;
@@ -30,6 +30,15 @@ module.controller('permissionmanagement.RoleMembersListController', [
       /* Maps user ids, present in the page, into user handles. */
       $scope.users = {};
       var loadUser = IdResolverService.getUserResolverFunction($scope.users);
+
+      // keep list of all members for client side filtering
+      var allMembers = [];
+
+      // criteria to filter members
+      $scope.filterCriteria = {
+        userId: '',
+        userHandle: ''
+      };
 
       /**
        * Return members which are selected in the table by checkboxes
@@ -58,6 +67,8 @@ module.controller('permissionmanagement.RoleMembersListController', [
               id: memberId
             }
           });
+          // save all members list for filtering
+          allMembers = _.clone($scope.members);
           // if have some members we will redraw table using footable plugin
           if ($scope.members.length) {
             // make sure changes to scope are applied
@@ -102,6 +113,7 @@ module.controller('permissionmanagement.RoleMembersListController', [
         member.isRemoving = true;
         return RoleService.unassignRole($stateParams.roleId, member.id).then(function() {
           _.remove($scope.members, { id: member.id });
+          _.remove(allMembers, { id: member.id });
           // we remove row of deleted member from footable table
           // which will also triggers footable table redraw
           // we don't worry to call it after $scope.members is updated so we don't use $timeout here
@@ -141,6 +153,93 @@ module.controller('permissionmanagement.RoleMembersListController', [
       }
 
       /**
+       * Helper function which redraws table with new member list
+       * and properly takes care about updating footable plugin
+       *
+       * The essential problem this function is solving is that after we
+       * update $scope.members, rows from the table are not being deleted immediately
+       * because we are using animations for rows.
+       * That's why if we try to force update footable plugin it still find old rows which are in
+       * process of animation and still in DOM tree.
+       * As a workaround in this function we disable animations for rows before updating them,
+       * thus footable plugin can see changes immediately after scope is updated.
+       *
+       * @param  {Array}    members  new array of members which has to displayed
+       * @param  {Function} callback optional callback after table is fully redrawn
+       *
+       * @return {Promise} resolved after table was completely updated
+       */
+      function redrawTableWithMembers(members) {
+        var $footable = $('.footable');
+
+        // disable animations for rows, so old rows will be removed immediately
+        // from the DOM tree and footable can see changes
+        $footable.find('tr').each(function(index, el) {
+          $animate.enabled(el, false);
+        });
+
+        // update members list in scope
+        $scope.members = members;
+
+        // make sure that changes are applied to the scope
+        return $timeout(function() {
+          // force footable plugin to redraw
+          $footable.trigger('footable_redraw');
+          // enable animation for table rows again
+          $footable.find('tr').each(function(index, el) {
+            $animate.enabled(el, true);
+          });
+        });
+      }
+
+      /**
+       * Applies filter to the member list
+       */
+      $scope.applyFilter = function() {
+        var filteredMembers = _.clone(allMembers);
+
+        // filter by ids first, it works immediately as we know all the data
+        // so we don't need to show loader for this
+        if ($scope.filterCriteria.userId) {
+          filteredMembers = _.filter(filteredMembers, { id: $scope.filterCriteria.userId });
+        }
+
+        // if handle filter is defined and we still have some rows to filter
+        if ($scope.filterCriteria.userHandle && filteredMembers.length > 0) {
+          // we show loader as we need to make request to the server
+          $scope.isLoading = true;
+
+          // As there is no server API to filter role members and we don't have
+          // user handles to filter, we first have to find user ids by it's handle
+          // and after we can filter users by id
+          UserService.find({
+            fields: 'id',
+            filter: 'handle=*' + $scope.filterCriteria.userHandle + '*&like=true',
+            limit: 1000000 // set big limit to make sure server returns all records
+          }).then(function(users) {
+            var foundIds = _.map(users, 'id');
+
+            filteredMembers = _.filter(filteredMembers, function(member) {
+              return _.includes(foundIds, member.id);
+            });
+
+            redrawTableWithMembers(filteredMembers).then(function() {
+              $scope.isLoading = false;
+            });
+          }).catch(function(error) {
+            // is any error occurs show it and leave table untouched
+            $scope.isLoading = false;
+            $alert.error(error.error, $rootScope);
+          });
+
+        // if we don't filter by handle which makes server request
+        // redraw table immediately
+        } else {
+          redrawTableWithMembers(filteredMembers);
+        }
+      }
+
+      /**
        * Uncheck all checkboxes
        */
       function uncheckAll() {
@@ -174,6 +273,7 @@ module.controller('permissionmanagement.RoleMembersListController', [
         }
       }
 
+      // init footable plugin
       angular.element(document).ready(function() {
         $('.footable').on({
           // we watch footable jquery plugin footable_page_filled event
