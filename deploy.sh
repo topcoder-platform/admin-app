229c492248f96dd9d46b4b4a8b4338f38c007066
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2019-06-14 19:44:25 +0530

    AWS configuration changes

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ BUCKET_URL=$1
 #AWS_SECRET_ACCESS_KEY=$(eval "echo \$${ENV}_AWS_SECRET_ACCESS_KEY")
 
 # aws s3 sync dist s3://${BUCKET_URL} --acl public-read --delete
-AWS_BUCKET=$BUCKET_URL AWS_KEY=$AWS_ACCESS_KEY_ID AWS_SECRET=$AWS_SECRET_ACCESS_KEY ./node_modules/.bin/gulp publish
+AWS_BUCKET=$BUCKET_URL ./node_modules/.bin/gulp publish
