adb2234c0077243827c8fea1dd250d3357437961
anonymousjaggu <31332424+anonymousjaggu@users.noreply.github.com>
2020-10-21 14:40:37 +0545

    changes not complete

--- src/app/terms/terms.new.controller.js
+++ src/app/terms/terms.new.controller.js
@@ -2,30 +2,38 @@
 
 var module = angular.module('supportAdminApp');
 
-module.controller('terms.NewTermsController', ['$rootScope','$scope', 'TermsService', 'Alert',
-'AGREEABILITY_TYPES', 'AGREE_FOR_DOCUSIGN_TEMPLATE', '$state',
-  function($rootScope, $scope, TermsService, $alert, agreeabilityTypeList, docusignTypeId, $state) {
+module.controller('terms.NewTermsController', ['$rootScope', '$scope', 'TermsService', 'Alert',
+  'AGREEABILITY_TYPES', 'AGREE_FOR_DOCUSIGN_TEMPLATE', 'ELECTRONICALLY_AGREEABLE_TEMPLATE', '$state',
+  function ($rootScope, $scope, TermsService, $alert, agreeabilityTypeList, docusignTypeId, electronicallyAgreeableId, $state) {
     // init variables
     $scope.newTerms = {};
     $scope.processing = false;
     $scope.agreeabilityTypes = agreeabilityTypeList;
     $scope.isDocuSignFieldEnabled = false;
-    
+    $scope.isChanged = false;
+    $scope.isUrlEnabled = false;
+
     // clear the alert
     $alert.clear();
 
+    TermsService.getTypes().then(function () {
+      
+    });
+
     // enable/disable the docu sign template id field
-    $scope.agreeabilityTypeChange = function(agreeabilityTypeId) {
-      $scope.isDocuSignFieldEnabled = docusignTypeId == agreeabilityTypeId;
-      if(!$scope.isDocuSignFieldEnabled) {
+    $scope.agreeabilityTypeChange = function (term) {
+      $scope.isChanged = true;
+      $scope.isDocuSignFieldEnabled = docusignTypeId == term.agreeabilityTypeId;
+      $scope.isUrlEnabled = electronicallyAgreeableId == term.agreeabilityTypeId;
+      if (!$scope.isDocuSignFieldEnabled) {
         $scope.newTerms.docusignTemplateId = '';
       }
     }
-    
-     // create new terms of use
-     $scope.createTermsOfUse = function () {
+
+    // create new terms of use
+    $scope.createTermsOfUse = function () {
       $scope.processing = true;
-      var entity = Object.assign({ }, $scope.newTerms);
+      var entity = Object.assign({}, $scope.newTerms);
 
       if (entity.docusignTemplateId === '') {
         delete entity.docusignTemplateId;
