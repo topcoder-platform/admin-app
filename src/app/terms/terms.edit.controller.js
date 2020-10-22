1a07fe11a3fcd10076c7ad6bdf7e73dbb9a4c9ff
Gus Costa <gustavo@gustavoalves.com.br>
2020-10-22 15:14:25 -0300

    adding rich text editor for terms

--- src/app/terms/terms.edit.controller.js
+++ src/app/terms/terms.edit.controller.js
@@ -7,60 +7,78 @@ var module = angular.module('supportAdminApp');
  */
 module.controller('terms.EditTermsController', ['$scope', '$rootScope', 'AGREEABILITY_TYPES', 'AGREE_FOR_DOCUSIGN_TEMPLATE', '$log',
   'TermsService', 'Alert', '$state', '$stateParams',
-    function ($scope, $rootScope, agreeabilityTypeList, docusignTypeId, $log, TermsService, $alert, $state, $stateParams) {
-      // init variables
-      $scope.processing = false;
-      $scope.editTerms = { };
-      $scope.agreeabilityTypes = agreeabilityTypeList;
-      $scope.isDocuSignFieldEnabled = false;
-      
-      // enable/disable the docu sign template id field
-      $scope.agreeabilityTypeChange = function(agreeabilityTypeId) {
-        $scope.isDocuSignFieldEnabled = docusignTypeId == agreeabilityTypeId;
-        if(!$scope.isDocuSignFieldEnabled) {
-          $scope.editTerms.docusignTemplateId = '';
-        }
+  function ($scope, $rootScope, agreeabilityTypeList, docusignTypeId, $log, TermsService, $alert, $state, $stateParams) {
+    // init variables
+    $scope.processing = false;
+    $scope.editTerms = {};
+    $scope.agreeabilityTypes = agreeabilityTypeList;
+    $scope.isDocuSignFieldEnabled = false;
+
+    // enable/disable the docu sign template id field
+    $scope.agreeabilityTypeChange = function (agreeabilityTypeId) {
+      $scope.isDocuSignFieldEnabled = docusignTypeId == agreeabilityTypeId;
+      if (!$scope.isDocuSignFieldEnabled) {
+        $scope.editTerms.docusignTemplateId = '';
       }
-
-      // clear the alert
-      $alert.clear();
-
-      // fetch initial data
-      if ($stateParams.termsId) {
-        TermsService.findTermsById($stateParams.termsId).then(function (data) {
-          $scope.editTerms = data;
-          $scope.isDocuSignFieldEnabled = docusignTypeId == $scope.editTerms.agreeabilityTypeId;
-        }).catch(function (error) {
-          $alert.error(error.error, $rootScope);
-        });
-      }
-
-      // save the terms of use changes
-      $scope.saveTermsOfUse = function () {
-        $scope.processing = true;
-        var entity = Object.assign({ }, $scope.editTerms);
-
-        Object.keys(entity).forEach(function (key) {
-          if (entity[key] == null || entity[key] === '') {
-            delete entity[key];
-          }
-        });
-
-        // delete the id
-        delete entity.id;
-
-        // delete the agreeabilityType desc only id is needed
-        if(entity.agreeabilityType) {
-          delete entity.agreeabilityType;
-        }
-        delete entity.agreed;
-        TermsService.editTerms($scope.editTerms.id, entity).then(function () {
-          $scope.processing = false;
-          $state.go('index.terms.list');
-        }).catch(function (error) {
-          $alert.error(error.error, $rootScope);
-          $scope.processing = false;
-        });
-      };
     }
+
+    // enable or desable the editor
+    $scope.showEditor = false;
+    $scope.toggleEditor = function () {
+      $scope.showEditor = !$scope.showEditor
+    }
+
+    // configs of the wysiwyg editor
+    $scope.tinyMceSettings = {
+      skin_url: '/node_modules/tinymce/skins/lightgray',
+      statusbar: false,
+      menubar: false,
+      browser_spellcheck: true,
+      source_view: true,
+      height: 400,
+      toolbar: ['undo redo | formatselect | bold italic underline strikethrough | forecolor backcolor | link | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent | table | removeformat'],
+      plugins: ['table link textcolor contextmenu'],
+    };
+
+    // clear the alert
+    $alert.clear();
+
+    // fetch initial data
+    if ($stateParams.termsId) {
+      TermsService.findTermsById($stateParams.termsId).then(function (data) {
+        $scope.editTerms = data;
+        $scope.isDocuSignFieldEnabled = docusignTypeId == $scope.editTerms.agreeabilityTypeId;
+      }).catch(function (error) {
+        $alert.error(error.error, $rootScope);
+      });
+    }
+
+    // save the terms of use changes
+    $scope.saveTermsOfUse = function () {
+      $scope.processing = true;
+      var entity = Object.assign({}, $scope.editTerms);
+
+      Object.keys(entity).forEach(function (key) {
+        if (entity[key] == null || entity[key] === '') {
+          delete entity[key];
+        }
+      });
+
+      // delete the id
+      delete entity.id;
+
+      // delete the agreeabilityType desc only id is needed
+      if (entity.agreeabilityType) {
+        delete entity.agreeabilityType;
+      }
+      delete entity.agreed;
+      TermsService.editTerms($scope.editTerms.id, entity).then(function () {
+        $scope.processing = false;
+        $state.go('index.terms.list');
+      }).catch(function (error) {
+        $alert.error(error.error, $rootScope);
+        $scope.processing = false;
+      });
+    };
+  }
 ]);
--- src/app/terms/terms.new.controller.js
+++ src/app/terms/terms.new.controller.js
@@ -2,30 +2,48 @@
 
 var module = angular.module('supportAdminApp');
 
-module.controller('terms.NewTermsController', ['$rootScope','$scope', 'TermsService', 'Alert',
-'AGREEABILITY_TYPES', 'AGREE_FOR_DOCUSIGN_TEMPLATE', '$state',
-  function($rootScope, $scope, TermsService, $alert, agreeabilityTypeList, docusignTypeId, $state) {
+module.controller('terms.NewTermsController', ['$rootScope', '$scope', 'TermsService', 'Alert',
+  'AGREEABILITY_TYPES', 'AGREE_FOR_DOCUSIGN_TEMPLATE', '$state',
+  function ($rootScope, $scope, TermsService, $alert, agreeabilityTypeList, docusignTypeId, $state) {
     // init variables
     $scope.newTerms = {};
     $scope.processing = false;
     $scope.agreeabilityTypes = agreeabilityTypeList;
     $scope.isDocuSignFieldEnabled = false;
-    
+
     // clear the alert
     $alert.clear();
 
     // enable/disable the docu sign template id field
-    $scope.agreeabilityTypeChange = function(agreeabilityTypeId) {
+    $scope.agreeabilityTypeChange = function (agreeabilityTypeId) {
       $scope.isDocuSignFieldEnabled = docusignTypeId == agreeabilityTypeId;
-      if(!$scope.isDocuSignFieldEnabled) {
+      if (!$scope.isDocuSignFieldEnabled) {
         $scope.newTerms.docusignTemplateId = '';
       }
     }
-    
-     // create new terms of use
-     $scope.createTermsOfUse = function () {
+
+    // enable or desable the editor
+    $scope.showEditor = false;
+    $scope.toggleEditor = function() {
+      $scope.showEditor = !$scope.showEditor
+    }
+
+    // configs of the wysiwyg editor
+    $scope.tinyMceSettings = {
+      skin_url: '/node_modules/tinymce/skins/lightgray',
+      statusbar: false,
+      menubar: false,
+      browser_spellcheck: true,
+      source_view: true,
+      height: 400,
+      toolbar: ['undo redo | formatselect | bold italic underline strikethrough | forecolor backcolor | link | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent | table | removeformat'],
+      plugins: ['table link textcolor contextmenu'],
+    };
+
+    // create new terms of use
+    $scope.createTermsOfUse = function () {
       $scope.processing = true;
-      var entity = Object.assign({ }, $scope.newTerms);
+      var entity = Object.assign({}, $scope.newTerms);
 
       if (entity.docusignTemplateId === '') {
         delete entity.docusignTemplateId;
